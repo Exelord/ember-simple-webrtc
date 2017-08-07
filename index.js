4aa7f83529d5682db84abc5f2179a11e0d460afb
Maciej Kwaśniak <kmaciek17@gmail.com>
2017-08-07 19:54:45 +0200

    SimpleWebRTC

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 /* eslint-env node */
 'use strict';
 
+const path = require('path');
+const Funnel = require('broccoli-funnel');
+const mergeTrees = require('broccoli-merge-trees');
+
 module.exports = {
-  name: 'ember-simple-webrtc'
+  name: 'ember-simple-webrtc',
+
+  included: function(app) {
+    this._super.included(app);
+    app.import('vendor/ember-simple-webrtc/simplewebrtc-with-adapter.bundle.js', {
+      using: [{ transformation: 'amd', as: 'simplewebrtc' }]
+    });
+  },
+
+  simpleWebRTCPath() {
+    return path.join(this.app.project.nodeModulesPath, 'simplewebrtc', 'out');
+  },
+
+  treeForVendor: function(tree) {
+    let trees = [tree];
+
+    trees.push(new Funnel(this.simpleWebRTCPath(), {
+      destDir: 'ember-simple-webrtc',
+      files: ['simplewebrtc-with-adapter.bundle.js']
+    }));
+
+    return mergeTrees(trees);
+  },
 };
